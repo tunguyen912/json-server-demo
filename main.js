6158f0cab0cad0fdba33d37e3a642d5e81175e27
Tu Nguyen <nguyenvoanhtu129@gmail.com>
2022-09-23 10:55:36 +0700

    echoo

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
+server.get('/echoo', (req, res) => {
   res.jsonp(req.query);
 });
 
